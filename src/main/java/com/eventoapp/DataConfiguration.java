8318f633d29022d9993ed0bb44bcf7087faeb126
CarolinaPlacencio <carolinasplacencio@gmail.com>
2022-03-23 10:38:05 +0100

    Database connection configuration

---
+++ src/main/java/com/eventoapp/DataConfiguration.java
@@ -0,0 +1,40 @@
+package com.eventoapp;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+@Configuration
+public class DataConfiguration {
+	
+	//Bean de conexao com o banco de dados
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/eventoapp");
+		dataSource.setUsername("root");
+		dataSource.setPassword("carolina123");
+		return dataSource;
+	}
+	
+	//Configuraçao de Hibernate
+	public JpaVendorAdapter jpaVendorAdapter() {
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setDatabase(Database.MYSQL);
+		//Mostrar em consola todas as actualizacoes feitas na base de dados
+		adapter.setShowSql(true);
+		//Permite ao Hibernate criar as tabelas automaticamente
+		adapter.setGenerateDdl(true);
+		//Dialeto que será utilizado
+		adapter.setDatabasePlatform("org.hibernate.dialext.MySqlDialect");
+		adapter.setPrepareConnection(true);
+		return adapter;
+	}	
+
+}
